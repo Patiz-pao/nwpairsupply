3d779af36f4d13196e2995e3a6c796097e0904af
Patipat <angpaopao55@gmail.com>
2024-11-02 14:19:00 +0700

    add detail product

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
-import { Layout, Navbar } from "../components";
+import { Navbar } from "../components";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- src/pages/homepage/home/index.tsx
+++ src/pages/homepage/home/index.tsx
@@ -16,12 +16,44 @@ const Home = () => {
               </h1>
               <h1 className="text-2xl mt-3">ทำไมต้องระบบล้างแอร์รถยนต์ NWP</h1>
               <div className="border-t border-black mt-3"></div>
+              <div className="mt-3 space-y-3">
+                <p>✅ ไม่ต้องป้อนรหัส ไม่บังคับซื้อน้ำยา ไม่ผูกมัดกับบริษัท</p>
+                <p>✅ ไม่ใช่เครื่องจีน ไม่ต้องกลัวปัญหาบริการหลังการขาย</p>
+                <p>✅ รับประกันซ่อมฟรี ! บริการดี ไม่ทิ้งลูกค้า</p>
+                <p>✅ มีเครื่องเก่า เอามาแลกซื้อเครื่องใหม่ได้</p>
+                <p>✅ มีบัตรเครดิต/ไม่มีบัตรเครดิต ก็ผ่อนได้</p>
+                <p>✅ บริการส่งฟรี ! ทั่วประเทศ พร้อมทีมฝึกอบรมการใช้งาน</p>
+              </div>
+              <button
+                type="button"
+                onClick={() =>
+                  window.open(
+                    "https://www.facebook.com/profile.php?id=100092277436950",
+                    "_blank"
+                  )
+                }
+                className="hidden lg:block text-white text-xl bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mt-3 w-[150px]"
+              >
+                สั่งซื้อ Click!
+              </button>
             </div>
             <img
               src={homesectiondescription.src}
-              alt=""
+              alt="Air conditioning system"
               className="lg:ml-10 w-4/5 mx-auto lg:w-2/5 mt-5 lg:mt-0"
             />
+            <button
+                type="button"
+                onClick={() =>
+                  window.open(
+                    "https://www.facebook.com/profile.php?id=100092277436950",
+                    "_blank"
+                  )
+                }
+                className="block lg:hidden justify-center items-center text-white text-xl bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mt-3"
+              >
+                สั่งซื้อ Click!
+              </button>
           </div>
         </div>
       </Layout>
